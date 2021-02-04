40d68b6e821d1f9642829952b2535278d27e6ebb
büşra okur <busraokur99@gmail.com>
2021-02-05 02:29:05 +0300

    Add project files.

---
+++ Dictionary/MyDictionary.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dictionary
+{
+     public class MyDictionary<K,V>
+    {
+        //array oluşturuldu
+        K[] key;
+        V[] value;
+        public MyDictionary()
+        {
+            //oluşturulan arraye yer açtık
+            key = new K [0];
+            value = new V [0];
+        }
+     public void Add(K keys,V values) 
+        {
+            //key ve value için girilecek değerlerin girilmesi için 1 arttırdık ve geçici elemana emanet ettik
+            K[] tempArrayKeys = key;
+            key = new K[key.Length + 1];
+
+            V[] tempArrayValues = value;
+            value = new V[value.Length + 1];
+
+            //emanet ettiğimiz verileri geri istedik
+            for (int i = 0; i < tempArrayKeys.Length && i < tempArrayValues.Length; i++)
+            {
+                key[i] = tempArrayKeys[i];
+                value[i] = tempArrayValues[i];
+            }
+
+            //en son girilen değer dahil hepsini bir araya almak için toparlıyoruz
+            key[key.Length - 1] = keys;
+            value[value.Length - 1] = values;
+
+        }
+        //veee sonucu görme zamanıııı:)))
+        public int k
+        {
+            get { return key.Length; }
+        }
+        public int v
+        {
+            get { return value.Length; }
+        }
+
+    }
+}
---
+++ Dictionary/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //LİSTELENECEK OLAN VERİLER(bu arada,belirtilen key yani int,string kalıbı oluşturulduktan sonra value yani değer girilir)
+            Dictionary<int, string> ogrenciler = new Dictionary<int, string>();
+            ogrenciler.Add(1," Ayşe");
+            ogrenciler.Add(2, " Emel");
+            ogrenciler.Add(3, " Ali");
+            ogrenciler.Add(4, " Kayra");
+            ogrenciler.Add(5, " Büşra");
+
+            Console.WriteLine(ogrenciler.Count);
+          
+
+        }
+    }
+}
